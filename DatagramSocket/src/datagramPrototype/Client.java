107e7a58b84508a2efa9a7c205cdabb5aa450b47
Michaela Lipovits <m.lipovits@gmail.com>
2013-12-09 09:16:21 +0100

    Kommentare verbessert

--- DatagramSocket/src/datagramPrototype/Client.java
+++ DatagramSocket/src/datagramPrototype/Client.java
@@ -4,16 +4,22 @@ import java.net.DatagramPacket;
 import java.net.DatagramSocket;
 import java.net.InetAddress;
 
+/**
+ * Klasse Client, die eine benachrichtigung vom Server empfängt
+ * @author mlipovits
+ * @version 2013-12-09
+ */
 public class Client {
 	public static void main(String[] args) throws Exception {
 		@SuppressWarnings("resource")
+		//datagramSocket mit port 4711 öffnen
 		DatagramSocket socket = new DatagramSocket( 4711 );
 		while ( true ){
-			// Auf Anfrage warten
+			// Auf Anfrage warten (DatagramPacket)
 			DatagramPacket packet = new DatagramPacket( new byte[1024], 1024 );
-			socket.receive( packet );
+			socket.receive( packet ); //tatsaechliches empfangen
 
-			// Empfänger auslesen
+			// Empfänger auslesen (brauchma nicht, td mal drin lassen weils nett is)
 
 			InetAddress address = packet.getAddress();
 			int         port    = packet.getPort();
