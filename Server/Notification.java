1f9875422d483deff17e8809237a13ad91c24603
Nanak Tattyrek <nanak@tattyrek.at>
2014-01-08 11:02:01 +0100

    added comments to classes Notification and Server

--- Server/Notification.java
+++ Server/Notification.java
@@ -1,11 +1,18 @@
 package Server;
-
+/**
+ * This class handles the Notifications
+ * @author nanak
+ *
+ */
 public class Notification {
  
 	private String name;
 	 
 	private String type;
-	 
+	/**
+	 * Returns the notification as a String
+	 * @return the notification
+	 */
 	public String getNotification() {
 		return null;
 	}
