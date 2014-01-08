94ec6856095115c2ec5d8c316536731458d807bf
tlins <tobias.lins@student.tgm.ac.at>
2014-01-08 11:31:52 +0100

    commands commented

--- src/Server/LoginCommand.java
+++ src/Server/LoginCommand.java
@@ -1,7 +1,15 @@
 package Server;
 
+/**
+ * The login command implementation
+ * @author Tobi
+ *
+ */
 public class LoginCommand extends User implements Command {
-
+	/**
+	 * executes the command
+	 * @param cmd command to execute
+	 */
 	@Override
 	public boolean execute(String cmd) {
 		// TODO Auto-generated method stub
