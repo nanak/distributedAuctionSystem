60c5bafcde85a647f95d1098d10b366fa3176124
tlins <tobias.lins@student.tgm.ac.at>
2014-01-13 22:22:26 +0100

    ex

--- src/Server/Server.java
+++ src/Server/Server.java
@@ -10,27 +10,27 @@ import java.util.concurrent.ExecutorService;
  *
  */
 public class Server{
-	 
+
 	private ExecutorService pool;
-	 
+
 	private WatchDog watchDog;
-	 
+
 	private DataManager data;
-	 
+
 	private ArrayList<Auction> auction;
-	 
+
 	private ArrayList<User> user;
-	 
+
 	private Runnable tcp;
-	
+
 	private CommandMapFactory commands;
 	/**
 	 * Constructor of the auction system server
 	 * @param tcp TCP Port
 	 */
 	public Server(int tcpport) {
-		
-		
+
+
 		data=new DataManager(auction);
 		auction=new ArrayList<Auction>();
 		data.loadData();
@@ -51,21 +51,19 @@ public class Server{
 					System.out.println("Server is saving data and shutting down");
 					System.exit(1);
 				} catch (IOException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
 				}
-				
+
 			}
 		};
 		Thread t=new Thread(check);
 		t.start();
 	}
-	
+
 	/**
 	 * Stops the listening and shuts down the server safely
 	 */
 	public void shutdown() {
-	 
+
 	}
 }
- 
+
