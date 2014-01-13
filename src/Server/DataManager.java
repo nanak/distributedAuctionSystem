1ca806271de9317ded398308030b5e63fd5bf2a6
DanielDimitrijevic <ddimitrijevic@student.tgm.ac.at>
2014-01-13 19:21:49 +0100

    Now there is no Exeption. The Stream will only open when the File is not emptry

--- src/Server/DataManager.java
+++ src/Server/DataManager.java
@@ -1,18 +1,24 @@
 package Server;
 
+import java.io.BufferedInputStream;
+import java.io.EOFException;
+import java.io.File;
 import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
+import java.sql.Time;
 import java.util.ArrayList;
 //still needs changes.
+import java.util.Date;
 
 /**
  * Manages the auctions and everything who needs to needs to be stored
+ * 
  * @author Daniel,Tobi
- *
+ * 
  */
 public class DataManager {
 
@@ -22,7 +28,9 @@ public class DataManager {
 
 	/**
 	 * can save and load the auctions from a file
-	 * @param list auctions to be saved and loaded
+	 * 
+	 * @param list
+	 *            auctions to be saved and loaded
 	 */
 	public DataManager(ArrayList<Auction> list) {
 		try {
@@ -32,7 +40,7 @@ public class DataManager {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
 		}
-		this.auctionlist=list;
+		this.auctionlist = list;
 	}
 
 	/**
@@ -41,10 +49,12 @@ public class DataManager {
 	public void saveData() {
 		try {
 			ObjectOutputStream save = new ObjectOutputStream(saveFile);
-			//System.out.println(save.);
+			// System.out.println(save.);s
 			save.writeObject(auctionlist);
+			save.writeInt(auctionlist.size());
+			save.flush();
 			save.close();
-		} catch ( IOException e ) {
+		} catch (IOException e) {
 			System.out.println("Could not be saved");
 		}
 
@@ -52,21 +62,23 @@ public class DataManager {
 
 	/**
 	 * Load the auctions back in the array
+	 * 
 	 * @throws FileNotFoundException
 	 */
 	@SuppressWarnings("unchecked")
 	public void loadData(){
 		try {
-			ObjectInputStream load=new ObjectInputStream(loadFile);
+			while(loadFile.available()>0){// only creates the Stream when the File is filled with Data
+			ObjectInputStream load = new ObjectInputStream(loadFile);
 			Object obj = load.readObject();
-			auctionlist=(ArrayList<Auction>)obj;
+			auctionlist = (ArrayList<Auction>) obj;
+			load.close();
+			}
 		} catch (IOException | ClassNotFoundException e) {
-			//e.printStackTrace();
+			e.printStackTrace();
+
 			System.out.println("File not found");
-			auctionlist=new ArrayList<Auction>();
+			auctionlist = new ArrayList<Auction>();
 		}
 	}
 }
-
-
-
