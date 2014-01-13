e44c4f32e810ba1016d58e9221db588009069e94
tlins <tobias.lins@student.tgm.ac.at>
2014-01-13 10:53:55 +0100

    client now runnable with parameters

--- src/Client/RunClient.java
+++ src/Client/RunClient.java
@@ -8,7 +8,12 @@ package Client;
  */
 public class RunClient {
 	public static void main (String [] args){
-		Client c=new Client("127.0.0.1", 1111, 1234);
+		if(args.length==3){
+			Client c=new Client(args[0], Integer.parseInt(args[1]), Integer.parseInt(args[2]));
+		}else{
+			Client c=new Client("127.0.0.1", 1111, 1234);
+		}
+		
 	}
 	 
 }
